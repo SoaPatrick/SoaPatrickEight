46fe8e1ad041731830cab27cc0f74401eb5e6b7e
Patrick <patrick.leisinger@gmail.com>
2020-09-10 22:59:23 +0200

    SP8-8 [UPDATE] add theme support for formats
    - remove gutenberg blocks
    - remove color options for gutenberg blocks
    - set image sizes

--- template-functions/setup.php
+++ template-functions/setup.php
@@ -26,17 +26,33 @@ if ( ! function_exists( 'soapatrickeight_setup' ) ) :
     // Add Theme Support for wide and full-width images.
     add_theme_support( 'align-wide' );
 
-		// add Image sizes
-    # add_image_size( 'full-hd, 1920, 1080, array( 'center', 'center' ) );
+    // Add Post Format Support
+    add_theme_support( 'post-formats', array( 'status', 'link', 'quote' ) );
+
+    // Remove default core Gutenberg Block Patterns
+    remove_theme_support( 'core-block-patterns' );
+
+    // Disable custom font sizes
+    add_theme_support( 'disable-custom-font-sizes' );
+
+    // Disable custom Color-Picker
+    add_theme_support( 'disable-custom-colors' );
+
+    // Define theme specific Gutenberg Block Colors
+    add_theme_support( 'editor-color-palette', array());
+
+    // remove support for gradients
+    add_theme_support( 'disable-custom-gradients' );
+    add_theme_support( 'editor-gradient-presets', array() );
 
     // change default image sizes
-    # update_option( 'thumbnail_size_w', 150 );
-    # update_option( 'thumbnail_size_h', 150 );
-    # update_option( 'thumbnail_crop', 1 );
-    # update_option( 'medium_size_h', 300 );
-    # update_option( 'medium_size_w', 300 );
-    # update_option( 'large_size_w', 600 );
-    # update_option( 'large_size_h', 600 );
+    update_option( 'thumbnail_size_w', 150 );
+    update_option( 'thumbnail_size_h', 150 );
+    update_option( 'thumbnail_crop', 1 );
+    update_option( 'medium_size_w', 750 );
+    update_option( 'medium_size_h', 0 );
+    update_option( 'large_size_w', 1500 );
+    update_option( 'large_size_h', 0 );
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
