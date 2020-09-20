0f1954068726513481d4bf2b8fd37055b8f7afc3
Patrick <patrick.leisinger@gmail.com>
2020-09-20 18:00:14 +0200

    SP8-20 [UPDATE] add new header image
    - optimize font size variables

---
+++ template-partials/layout/header-sp8.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Layout part for displaying the header for soapatrickeight
+ *
+ * @package soapatrickeight
+ */
+
+?>
+
+<header class="site-header--not-home">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2135 447" class="bubbles-row">
+    <path class="bubbles-row__first" d="M0,268.54C25.46,257.91,65,248,124,248c132,0,152.88,66.43,287.08,63.33C584.08,307.33,632,182,803,182.17,947,182.3,1118,447,1292,447c175,0,297-171.06,483-174,190-3,227,78,360,101V0H0Z"/>
+  </svg>
+</header>
