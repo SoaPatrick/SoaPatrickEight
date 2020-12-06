53a5f4599b39b3ea05a71206bb79cdf494eda750
SoaPatrick <soapatrick@macbox.home>
2020-12-06 14:23:16 +0100

    SP8-66 [UPDATE] add some changes to navigation
    - reorder mobile navigation items
    - add privacy to main navigation
    - change settings icons for nav position

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
         ?>
 
         <p class="site-footer__copyright">
-          <?php echo sprintf( __( 'Stuff from 2000 to %s by SoaPatrick<a href="%s">Eight</a>', 'soapatrickeight' ), date('Y'), esc_url( home_url( '/log' )) ); ?> / <a href="<?php echo esc_url( home_url( '/' ) ); ?>privacy-policy" aria-label="<?php esc_html_e( 'Privacy', 'soapatrickeight' ); ?>"><?php esc_html_e( 'Privacy', 'soapatrickeight' ); ?></a>
+          <?php echo sprintf( __( 'Stuff from 2000 to %s by SoaPatrick<a href="%s">Eight</a>', 'soapatrickeight' ), date('Y'), esc_url( home_url( '/log' )) ); ?></a>
         </p>
       </div>
     </footer>
